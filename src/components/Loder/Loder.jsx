2c567380a7ef938f791ba3d5b507f8158151f8f6
shweta <shwetakidile@gmail.com>
2025-04-30 21:08:57 +0530

    Fix mobile admin nav route for Add Book

--- src/components/Loder/Loder.jsx
+++ src/components/Loder/Loder.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Loder = () => {
   return (
     
-<div class="flex flex-row gap-2">
-  <div class="animate-pulse bg-gray-300 w-12 h-12 rounded-full"></div>
-  <div class="flex flex-col gap-2">
-    <div class="animate-pulse bg-gray-300 w-28 h-5 rounded-full"></div>
-    <div class="animate-pulse bg-gray-300 w-36 h-5 rounded-full"></div>
+<div className="flex flex-row gap-2">
+  <div className="animate-pulse bg-gray-300 w-12 h-12 rounded-full"></div>
+  <div className="flex flex-col gap-2">
+    <div className="animate-pulse bg-gray-300 w-28 h-5 rounded-full"></div>
+    <div className="animate-pulse bg-gray-300 w-36 h-5 rounded-full"></div>
   </div>
 </div>
 
